01d99f696e0ef575d7d6088bc779dcdab5821288
buuufy <s1mpleworkpost@gmail.com>
2022-11-26 20:44:51 +0200

    Initial commit

---
+++ src/main/java/com/example/studentnauczyciel/StudentNauczycielApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentnauczyciel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentNauczycielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentNauczycielApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentnauczyciel/StudentNauczycielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentnauczyciel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentNauczycielApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
